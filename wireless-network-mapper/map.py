f74b93a662468d660e59eb38c4c3ac938aa4e163
bernardtran99 <bernardtran99@gmail.com>
2021-12-14 05:21:51 -0500

    Update map.py

--- wireless-network-mapper/map.py
+++ wireless-network-mapper/map.py
@@ -13,7 +13,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def web_page():
-    return "Starting app..."
+    html_raw = "<h1>Wireless Network Mapper</h1>"
+    return html_raw
 
 if __name__ == "__main__":
     app.run("0.0.0.0")
