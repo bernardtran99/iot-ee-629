a9482286b6e3c6061e878a962c908b857fb8abc8
bernardtran99 <bernardtran99@gmail.com>
2021-12-14 06:29:57 -0500

    Update process.py

--- wireless-network-mapper/process.py
+++ wireless-network-mapper/process.py
@@ -10,10 +10,16 @@ network_list = {}
 result = subprocess.Popen(["sudo","iwlist","wlan0","scan"],stdout=subprocess.PIPE, universal_newlines=True)
 output, error = result.communicate()
 
+
+
 for line in output.split("\n"):
+    signal_level = 0
+    if re.search('Signal level',line):
+        signal = re.search('Signal level=(.*) dbm',line)
+        signal_level = signal.group(1)
     if re.search('ESSID',line):
         essid = re.search('ESSID:"(.*)"',line)
-        network_list[essid.group(1)] = 0
+        network_list[essid.group(1)] = signal_level
         # print(essid.group(1))
 
 print(network_list)
