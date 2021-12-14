0e47d7baa97c10eed210a6050a454e9f31cb4207
bernardtran99 <bernardtran99@gmail.com>
2021-12-14 08:39:09 -0500

    Update process.py

--- wireless-network-mapper/process.py
+++ wireless-network-mapper/process.py
@@ -12,21 +12,10 @@ previous_list = {}
 
 display_table = {}
 
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template('table.html', display_table=display_table)
-
-if __name__ == "__main__":
-    app.run("0.0.0.0")
-
-times = 0
-
-while times < 10:
+def get_table():
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
-    network_list["ESSID"] = current_time
+    network_list["TIME"] = current_time
 
     result = subprocess.Popen(["sudo","iwlist","wlan0","scan"],stdout=subprocess.PIPE, universal_newlines=True)
     output, error = result.communicate()
@@ -42,14 +31,48 @@ while times < 10:
             essid = re.search('ESSID:"(.*)"',line)
             network_list[essid.group(1)] = signal_level
             # print(essid.group(1))
+
+    return network_list
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    display_table = get_table()
+    return render_template('table.html', display_table=display_table)
+
+if __name__ == "__main__":
+    app.run("0.0.0.0")
+
+# times = 0
+
+# while times < 10:
+#     now = datetime.now()
+#     current_time = now.strftime("%H:%M:%S")
+#     network_list["TIME"] = current_time
+
+#     result = subprocess.Popen(["sudo","iwlist","wlan0","scan"],stdout=subprocess.PIPE, universal_newlines=True)
+#     output, error = result.communicate()
+
+#     for line in output.split("\n"):
+
+#         if re.search('Signal',line):
+#             signal = re.search('Signal level=(.*) dBm',line)
+#             signal_level = []
+#             signal_level = signal.group(1)
+        
+#         if re.search('ESSID',line):
+#             essid = re.search('ESSID:"(.*)"',line)
+#             network_list[essid.group(1)] = signal_level
+#             # print(essid.group(1))
     
-    display_table = network_list
+#     display_table = network_list
 
-    times += 1
-    print("Run: ", times)
-    print(network_list)
-    print("\n")
+#     times += 1
+#     print("Run: ", times)
+#     print(network_list)
+#     print("\n")
 
-    previous_list = network_list
+#     previous_list = network_list
 
-    time.sleep(10)
+#     time.sleep(10)
