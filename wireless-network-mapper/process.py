37a5580ba93d2f348e75f1ae67bbaf4328ea48af
bernardtran99 <bernardtran99@gmail.com>
2021-12-14 08:12:46 -0500

    Update process.py

--- wireless-network-mapper/process.py
+++ wireless-network-mapper/process.py
@@ -3,22 +3,34 @@ import time
 import subprocess
 import os
 import re
-
-network_list = {}
-
-
-result = subprocess.Popen(["sudo","iwlist","wlan0","scan"],stdout=subprocess.PIPE, universal_newlines=True)
-output, error = result.communicate()
+from datetime import datetime
 
 
 
-for line in output.split("\n"):
-    if re.search('Signal',line):
-        signal = re.search('Signal level=(.*) dBm',line)
-        signal_level = signal.group(1)
-    if re.search('ESSID',line):
-        essid = re.search('ESSID:"(.*)"',line)
-        network_list[essid.group(1)] = signal_level
-        # print(essid.group(1))
+network_list = {"ESSID":[]}
 
-print(network_list)
+times = 0
+
+while times < 10:
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    network_list["ESSID"].append(current_time)
+
+    result = subprocess.Popen(["sudo","iwlist","wlan0","scan"],stdout=subprocess.PIPE, universal_newlines=True)
+    output, error = result.communicate()
+
+    for line in output.split("\n"):
+
+        if re.search('Signal',line):
+            signal = re.search('Signal level=(.*) dBm',line)
+            signal_level = []
+            signal_level = signal.group(1)
+        
+        if re.search('ESSID',line):
+            essid = re.search('ESSID:"(.*)"',line)
+            network_list[essid.group(1)] = signal_level
+            # print(essid.group(1))
+
+    print(network_list)
+    times += 1
+    time.sleep(10)
