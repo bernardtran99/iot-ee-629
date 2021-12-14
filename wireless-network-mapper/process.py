efb251da0155b6bb98cc0920402dc23d51f42228
bernardtran99 <bernardtran99@gmail.com>
2021-12-14 06:15:11 -0500

    Update process.py

--- wireless-network-mapper/process.py
+++ wireless-network-mapper/process.py
@@ -10,6 +10,6 @@ networkList = ()
 result = subprocess.Popen(["sudo","iwlist","wlan0","scan"],stdout=subprocess.PIPE, universal_newlines=True)
 output, error = result.communicate()
 
-for line in output:
+for line in output.split("\n"):
     if re.search("ESSID",line):
         print(line)
