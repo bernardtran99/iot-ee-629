6e0bc617c7c7dd52f9c6c6de1f9579153a7b4776
bernardtran99 <bernardtran99@gmail.com>
2021-12-14 08:45:42 -0500

    Update process.py

--- wireless-network-mapper/process.py
+++ wireless-network-mapper/process.py
@@ -6,10 +6,7 @@ import re
 from datetime import datetime
 from flask import *
 
-network_list = {"ESSID":0}
-
-previous_list = {}
-
+network_list = {}
 display_table = {}
 
 def get_table():
@@ -43,36 +40,3 @@ def index():
 
 if __name__ == "__main__":
     app.run("0.0.0.0")
-
-# times = 0
-
-# while times < 10:
-#     now = datetime.now()
-#     current_time = now.strftime("%H:%M:%S")
-#     network_list["TIME"] = current_time
-
-#     result = subprocess.Popen(["sudo","iwlist","wlan0","scan"],stdout=subprocess.PIPE, universal_newlines=True)
-#     output, error = result.communicate()
-
-#     for line in output.split("\n"):
-
-#         if re.search('Signal',line):
-#             signal = re.search('Signal level=(.*) dBm',line)
-#             signal_level = []
-#             signal_level = signal.group(1)
-        
-#         if re.search('ESSID',line):
-#             essid = re.search('ESSID:"(.*)"',line)
-#             network_list[essid.group(1)] = signal_level
-#             # print(essid.group(1))
-    
-#     display_table = network_list
-
-#     times += 1
-#     print("Run: ", times)
-#     print(network_list)
-#     print("\n")
-
-#     previous_list = network_list
-
-#     time.sleep(10)
