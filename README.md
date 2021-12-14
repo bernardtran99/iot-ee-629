625656ce281179e34380f9bda4e6f8ecdb56f3d9
bernardtran99 <bernardtran99@gmail.com>
2021-12-14 05:02:09 -0500

    update all

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # iot-ee-629
- 
+
 EE 629
 Internet Of Things
 Bernard Tran
+
+To run Wireless Netowrk Mapper:
+
---
+++ wireless-network-mapper/app.py
@@ -0,0 +1,19 @@
+
+#Wireless Network Mapper
+#Bernard Tran
+
+import sys
+import time
+import subprocess
+from flask import Flask
+
+network_dict = {}
+
+app = Flask(__name__)
+
+@app.route("/")
+def web_page():
+    return "Starting app..."
+
+if __name__ == "__main__":
+    app.run()
