ed773b8a1d26147cd07a7e48346f71e0059f9782
bernardtran99 <bernardtran99@gmail.com>
2021-12-14 06:05:46 -0500

    Update process.py

--- wireless-network-mapper/process.py
+++ wireless-network-mapper/process.py
@@ -9,4 +9,4 @@ networkList = ()
 result = subprocess.Popen(["sudo","iwlist","wlan0","scan"],stdout=subprocess.PIPE, universal_newlines=True)
 output, error = result.communicate()
 
-# print(output)
+print(output)
